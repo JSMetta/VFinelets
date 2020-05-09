22e0822536340d3f4c65647208e4a860c7905c5c
clx1608@qq.com <hbdennischen@outlook.com>
2020-05-09 15:31:11 +0800

    enhance for development mode

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,21 +1,49 @@
 'use strict'
-
-const {
-    VueLoaderPlugin
-} = require('vue-loader')
+const path = require('path'),
+    HtmlWebpackPlugin = require('html-webpack-plugin'),
+    {
+        CleanWebpackPlugin
+    } = require('clean-webpack-plugin'),
+    {
+        VueLoaderPlugin
+    } = require('vue-loader')
 
 module.exports = {
     mode: 'development',
-    entry: [
-        './src/app.js'
-    ],
+    entry: {
+        app: './src/app.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     module: {
         rules: [{
-            test: /\.vue$/,
-            use: 'vue-loader'
-        }]
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+        ]
     },
     plugins: [
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: '纺织品自动化疵点检测操作系统',
+            template: 'index.html'
+        }),
     ]
 }
+
+
+// TODO: Deploy dist to server by using webpack-dev-middleware
