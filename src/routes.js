44f4661b9b4b0595d94b34b778c948e363e5d082
clx1608@qq.com <hbdennischen@outlook.com>
2020-05-13 13:12:38 +0800

    finelets project start point

--- src/routes.js
+++ src/routes.js
@@ -1,46 +1,6 @@
 import Home from './pages/home.vue'
-import DevHome from './pages/dev/Home.vue'
-import Instructions from './pages/dev/Instructions.vue'
-import RunProgram from './pages/dev/RunProgram.vue'
-
-import OprHome from './pages/opr/Home.vue'
-
-const ROLE_DEV = 'DEV'
-const ROLE_OPR = 'OPR'
 
 const routes = [
-  {
-    path: '/dev/runprogram',
-    name: 'runProgram',
-    meta: {
-      role: ROLE_DEV
-    },
-    component: RunProgram
-  },
-  {
-    path: '/dev/home',
-    name: 'devHome',
-    meta: {
-      role: ROLE_DEV
-    },
-    component: DevHome
-  },
-  {
-    path: '/dev/instructions',
-    name: 'Instructions',
-    meta: {
-      role: ROLE_DEV
-    },
-    component: Instructions
-  },
-  {
-    path: '/opr/home',
-    name: 'oprHome',
-    meta: {
-      role: ROLE_OPR
-    },
-    component: OprHome
-  },
   {
     path: '/',
     name: 'home',
@@ -50,8 +10,6 @@ const routes = [
 ]
 
 const starts = {
-  OPR: 'oprHome',
-  DEV: 'devHome'
 }
 
 export default {
