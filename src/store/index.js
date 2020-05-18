93a8f50c44ce640e1feac820c852a7bfb3de43fd
clx1608@qq.com <hbdennischen@outlook.com>
2020-05-18 15:16:18 +0800

    DevHome and RunProgram pages

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import states from './states'
 import mutations from './mutations'
 import getters from './getters'
 
-const items = []
+const items = ['selectedProgram']
 
 const config = {
   items, states, getters, mutations, actions  
