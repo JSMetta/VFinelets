e7af9fd7cadafe40bbf167af92f8c9d6e48de688
clx1608@qq.com <hbdennischen@outlook.com>
2020-05-18 15:16:18 +0800

    DevHome and RunProgram pages

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 import Home from './pages/home.vue'
+import RunProgram from './pages/dev/RunProgram.vue'
+import DevHome from './pages/dev/Home.vue'
+
+const ROLE_DEV = 'DEV'
 
 const routes = [
   {
@@ -6,10 +10,27 @@ const routes = [
     name: 'home',
     meta: {public: true},
     component: Home
+  },
+  {
+    path: '/dev/runprogram',
+    name: 'runProgram',
+    meta: {
+      role: ROLE_DEV
+    },
+    component: RunProgram
+  },
+  {
+    path: '/dev/home',
+    name: 'devHome',
+    meta: {
+      role: ROLE_DEV
+    },
+    component: DevHome
   }
 ]
 
 const starts = {
+  DEV: 'devHome'
 }
 
 export default {
