3a483dbbef129bb2b1970d465f3514483b8c1bfa
clx1608@qq.com <hbdennischen@outlook.com>
2020-06-04 19:12:01 +0800

    only store user and token in Vuex

--- finelets/store/CreateStore.js
+++ finelets/store/CreateStore.js
@@ -8,15 +8,17 @@ import __mutations from './mutations'
 import __states from './states'
 
 Vue.use(Vuex)
+// localStorage.clear()
 const vuexLocalStorage = new VuexPersist({
   key: 'vuex', // The key to store the state on in the storage provider.
-  storage: window.localStorage // or window.sessionStorage or localForage
+  storage: window.localStorage, // or window.sessionStorage or localForage
+  reducer: (state) => ({ user: state.user, token: state.token }),
   // Function that passes the state and returns the state with only the objects you want to store.
   // reducer: state => state,
   // Function that passes a mutation and lets you decide if it should update the state in localStorage.
   // filter: mutation => (true)
 })
-const __items = ['entry', 'token', 'user', 'selectedUser']
+const __items = ['entry', 'token', 'user', 'selectedUser', 'avatar']
 
 function createGetters (items) {
   const obj = {}
